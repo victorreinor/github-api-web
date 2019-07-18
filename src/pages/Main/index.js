aeeb54bf2ba0a046c7f34ab2b46c7ae0b756c1fd
Victor Reinor <victorreinor@gmail.com>
2019-07-18 20:41:22 -0300

    adiciona a estrutura da pagina main e seus componentes estilizados

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>Repositórios</h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
